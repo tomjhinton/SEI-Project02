6857f3792ff035e17297f615b5d7b81805a23f82
Stuart Mees <stuartmees@gmail.com>
2019-04-23 16:45:28 +0100

    video embedded in movie show

--- src/components/GetTrailer.js
+++ src/components/GetTrailer.js
@@ -8,30 +8,40 @@ class GetTrailer extends React.Component {
     super(props)
 
     this.state = {
-      film: null //WHY NULL?
+      moviedbID: null, //WHY NULL?
+      youtubeID: null
     }
   }
 
   componentDidMount() {
-    console.log(this.props.id)
-    console.log(`https://api.themoviedb.org/3/find/${this.props.id}?api_key=205882c0653c77431db40e15ec7fd210&external_source=imdb_id`)
+
     axios.get(`https://api.themoviedb.org/3/find/${this.props.id}?api_key=205882c0653c77431db40e15ec7fd210&external_source=imdb_id`)
       .then(res => {
-        this.setState({ film: res.data })
-        console.log(res)
+        this.setState({ moviedbID: res.data.movie_results[0].id })
+        console.log(this.state.moviedbID)
+
+        axios.get(`http://api.themoviedb.org/3/movie/${this.state.moviedbID}/videos?api_key=205882c0653c77431db40e15ec7fd210`)
+          .then(res => {
+            this.setState({youtubeID: res.data.results[0].key})
+            console.log(this.state.youtubeID)
+
+          })
       })
-    console.log(this)
   }
 
   render(){
-    if(!this.state.film) return null
     return (
+
       <div>
-        Hi
+        {this.state.moviedbID && this.state.youtubeID &&
+          <iframe width="560" height="315" src={`https://www.youtube.com/embed/${this.state.youtubeID}`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        }
+        <a href={`http://www.youtube.com/watch?v=${this.state.youtubeID}`}> Trailer </a>
       </div>
+
+
     )
   }
-
 }
 
 export default GetTrailer
